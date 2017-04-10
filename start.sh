ff2c00f43874eea07b88ca8dc3f6bb81d2456ef1
Jan Knedlik <j.knedlik@gsi.de>
2017-04-10 12:13:50 +0200

    fixed start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker rm xrootdBuilder &>/dev/null
-docker build  --build-arg XRD_VER=4.4.1 -t xrootdbuilder:0.1 -t xrootdbuilder:latest . 
+docker build  --build-arg XRD_VER=4.5.0 -t xrootdbuilder:0.1 -t xrootdbuilder:latest .
 docker run -v $PWD/build:/xrdinstall/vol  --name "xrootdBuilder" -i -t xrootdbuilder:latest
