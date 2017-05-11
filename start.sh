b387ce296489054b610bd3265aabe4241ab49bbb
Sören Fleischer <s.fleischer@gsi.de>
2017-05-11 14:27:27 +0200

    added LimitNOFILE=65536 to xrootd unit file

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker rm xrootdBuilder &>/dev/null
-docker build --tag xrootdbuilder:0.1 --tag xrootdbuilder:latest --build-arg XRD_VER="4.5.0" --build-arg ADDITIONAL_VERSION_STRING="-gsi-1" .
+docker build --tag xrootdbuilder:0.1 --tag xrootdbuilder:latest --build-arg XRD_VER="4.5.0" --build-arg ADDITIONAL_VERSION_STRING="-gsi-2" .
 docker run -v $PWD/build:/xrdinstall/vol  --name "xrootdBuilder" -i -t xrootdbuilder:latest
